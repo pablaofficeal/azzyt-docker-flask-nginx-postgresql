e37cbcf7dac2cce00a225f2a07ccd31664f31f22
pablaofficeal <pavlogorosko56@gmail.com>
2025-12-04 15:58:49 +0100

    Удалены неиспользуемые маршруты и добавлены импорты для работы с базой данных

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
+from models.main_routers_db import *
+from models.imp import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
+#db.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
